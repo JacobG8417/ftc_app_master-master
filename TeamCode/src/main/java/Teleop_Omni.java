30581a555491727ae4cad785074ad13c6017c597
JacobGowan99bacon <45701922+JacobGowan99bacon@users.noreply.github.com>
2019-11-22 19:37:42 -0500

    stopped tne opmode from going twice and moved the gamepad stuff

--- TeamCode/src/main/java/Teleop_Omni.java
+++ TeamCode/src/main/java/Teleop_Omni.java
@@ -5,26 +5,19 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-public class Teleop_Omni {
-
-    @TeleOp(name="Teleop_Omni", group="TeleOp")
-    public class OmniTeleop extends LinearOpMode {
-
-        // public void Driver() {
-        private DcMotor leftDrive;
-        private DcMotor rightDrive;
-
-        // private Gamepad gamepad1 = new Gamepad;
+@TeleOp(name="Teleop_Omni", group="TeleOp")
+public class Teleop_Omni extends LinearOpMode {
 
         @Override
         public void runOpMode() throws InterruptedException {
             waitForStart();
             while(opModeIsActive()){
+                double power = gamepad1.right_stick_y;
+                double rightmotorpower = gamepad1.right_stick_y;
+                double leftmotorpower = gamepad1.left_stick_y;
                 this.telemetry.update();
 
                 telemetry.addData("Status", "Initialized");
-
-
 
                 DcMotor front_left = hardwareMap.get(DcMotor.class,"front_left");
                 DcMotor front_right = hardwareMap.get(DcMotor.class, "front_right");
@@ -42,15 +35,9 @@ public class Teleop_Omni {
             waitForStart();
 
             // run until the end of the match (driver presses STOP)
-            while (opModeIsActive()) {
-                double power = gamepad1.right_stick_y;
-                double rightmotorpower = gamepad1.right_stick_y;
-                double leftmotorpower = gamepad1.left_stick_y;
 
-                leftDrive.setPower(leftmotorpower);
-                rightDrive.setPower(rightmotorpower);
                 telemetry.addData("Status", "Running");
-                telemetry.update();
+                final boolean update = telemetry.update();
 
 
             }
@@ -58,4 +45,4 @@ public class Teleop_Omni {
         }
     }
 
-}}
+
